9965472521df127b8626eb1e329e7f7f991f64e9
Yuliawan Rizka Syafaat <yuliawanrizka.syafaat@mitrais.com>
2017-09-12 10:58:46 +0800

    Initial push

---
+++ src/main/java/com/mitrais/trainingadminservice/TrainingadminserviceApplication.java
@@ -0,0 +1,12 @@
+package com.mitrais.trainingadminservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainingadminserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainingadminserviceApplication.class, args);
+	}
+}
