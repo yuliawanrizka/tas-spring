9e11278165aabd55820bf055fe3630b208272fd8
Yuliawan Rizka Syafaat <yuliawanrizka.syafaat@mitrais.com>
2017-09-13 16:19:10 +0800

    Authentication Controller skeleton

---
+++ src/main/java/com/mitrais/trainingadminservice/controller/AuthenticationController.java
@@ -0,0 +1,28 @@
+/*
+ * @(#) AuthenticationController.java, v 1.0 2017/09/13 16:00:27
+ * 
+ * Copyright (c) 2017, PT. Mitrais, Bali, Indonesia.
+ * All rights reserved.
+ * 
+ * Revision History
+ * 
+ * 13-Sep-2017 Yuliawan Rizka Syafaat             [1.0]-Initial Coding
+ * 
+ */
+
+package com.mitrais.trainingadminservice.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/**
+ * Class Description
+ * 
+ */
+@RestController
+@RequestMapping("/auth")
+public class AuthenticationController {
+
+    
+}
