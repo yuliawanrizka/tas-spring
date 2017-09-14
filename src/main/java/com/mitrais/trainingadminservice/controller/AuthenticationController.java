90382b072889a471df55f2dcd1bcb0b70e080f96
Yuliawan Rizka Syafaat <yuliawanrizka.syafaat@mitrais.com>
2017-09-14 16:32:28 +0800

    try using sha512 for hash

--- src/main/java/com/mitrais/trainingadminservice/controller/AuthenticationController.java
+++ src/main/java/com/mitrais/trainingadminservice/controller/AuthenticationController.java
@@ -18,8 +18,12 @@ import com.mitrais.trainingadminservice.repository.EmployeeDataRepository;
 import com.mitrais.trainingadminservice.repository.TokenRepository;
 import com.mitrais.trainingadminservice.request.AuthenticationRequest;
 import com.mitrais.trainingadminservice.response.AuthenticationResponse;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.sql.Timestamp;
+import java.util.Base64;
 import org.apache.commons.lang3.RandomStringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -43,12 +47,9 @@ public class AuthenticationController {
     private EmployeeDataRepository employeeData;
     
     @PostMapping(value = "")
-    public ResponseEntity login(@RequestBody final AuthenticationRequest request) {
+    public ResponseEntity login(@RequestBody final AuthenticationRequest request) throws NoSuchAlgorithmException, UnsupportedEncodingException {
         EmployeeData employee = employeeData.findByAccountName(request.getUsername());
-        
-        //need hash function!!
-        
-        String passwordCompare = employee.getSalt() + request.getPassword();
+        String passwordCompare = Encode_SHA512(employee.getSalt() + request.getPassword());
         if (employee.getPassword() == passwordCompare ) {
             AuthenticationResponse response = new AuthenticationResponse();
             Token tokenEmployee = new Token();
@@ -77,5 +78,11 @@ public class AuthenticationController {
         char[] possibleCharacters = (new String("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789~!@#$%^&*-_=")).toCharArray();
         String randomStr = RandomStringUtils.random( randomStrLength, 0, possibleCharacters.length-1, false, false, possibleCharacters, new SecureRandom() );
         return randomStr;
+    }
+    private String Encode_SHA512(String input) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        MessageDigest md = MessageDigest.getInstance("SHA-512");
+        byte [] inputBytes = input.getBytes("UTF-16LE");
+        byte[] digest = md.digest(inputBytes);
+        return Base64.getEncoder().encodeToString(digest);
     }
 }
