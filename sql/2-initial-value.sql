a9784d080d34f71b0952d64ee388587d1af805ff
Yuliawan Rizka Syafaat <yuliawanrizka.syafaat@mitrais.com>
2017-09-25 09:14:06 +0800

    change the script

--- sql/2-initial-value.sql
+++ sql/2-initial-value.sql
@@ -1,5 +1,27 @@
--- Roles table init --
+-- Delete Data First --
+DELETE FROM [UserRole];
+DELETE FROM [Employee];
+DELETE FROM [Course];
+DELETE FROM [Location];
+DELETE FROM [Grade];
 DELETE FROM [Roles];
+
+-- Reseed Primary Key --
+DBCC CHECKIDENT ('[UserRole]', RESEED, 1);
+GO
+DBCC CHECKIDENT ('[Employee]', RESEED, 1);
+GO
+DBCC CHECKIDENT ('[Course]', RESEED, 1);
+GO
+DBCC CHECKIDENT ('[Location]', RESEED, 1);
+GO
+DBCC CHECKIDENT ('[Grade]', RESEED, 1);
+GO
+DBCC CHECKIDENT ('[Roles]', RESEED, 1);
+GO
+
+
+-- Roles table init --
 SET IDENTITY_INSERT[Roles] ON;
 
 INSERT INTO [Roles](roleId, roleName) VALUES (1, 'Administrator');
@@ -10,19 +32,48 @@ INSERT INTO [Roles](roleId, roleName) VALUES (4, 'Staff');
 SET IDENTITY_INSERT [Roles] OFF;
 
 -- Grade table init --
-DELETE FROM [Grade];
 SET IDENTITY_INSERT[Grade] ON;
 
-INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (1, 'SE', 'JP');
-INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (2, 'SE', 'PG');
-INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (3, 'SE', 'AP');
-INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (4, 'SE', 'AN');
-INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (5, 'ADM', 'ADM1');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (1, 'ADM', 'ADM1');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (2, 'ADM', 'ADM2');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (3, 'ADM', 'ADM3');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (4, 'ADM', 'ADM4');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (5, 'IT', 'IT1');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (6, 'IT', 'IT2');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (7, 'IT', 'IT3');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (8, 'IT', 'IT4');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (9, 'MJF', 'MG1');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (10, 'MJF', 'MG2');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (11, 'MJF', 'SM');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (12, 'MJF', 'VP');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (13, 'FA', 'FA1');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (14, 'FA', 'FA2');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (15, 'FA', 'FA3');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (16, 'FA', 'FA4');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (17, 'TR', 'TR1');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (18, 'TR', 'TR2');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (19, 'TR', 'TR3');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (20, 'TR', 'TR4');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (21, 'SE', 'JP');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (22, 'SE', 'PG');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (23, 'SE', 'AP');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (24, 'SE', 'AN');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (25, 'SQ', 'JT');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (26, 'SQ', 'TS');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (27, 'SQ', 'ST');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (28, 'SQ', 'TA');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (29, 'CON', 'JC');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (30, 'CON', 'CON');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (31, 'CON', 'SC');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (32, 'CON', 'LC');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (33, 'DSG', 'JD');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (34, 'DSG', 'DS');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (35, 'DSG', 'SD');
+INSERT INTO [Grade](gradeId, jobFamily, grade) VALUES (36, 'DSG', 'DD');
 
 SET IDENTITY_INSERT [Grade] OFF;
 
 -- Location table init --
-DELETE FROM [Location];
 SET IDENTITY_INSERT[Location] ON;
 
 INSERT INTO [Location](locationId, location) VALUES ( 1, 'Bali');
@@ -33,7 +84,6 @@ INSERT INTO [Location](locationId, location) VALUES ( 4, 'Jakarta');
 SET IDENTITY_INSERT [Location] OFF;
 
 -- Course table init --
-DELETE FROM [Course];
 SET IDENTITY_INSERT[Course] ON;
 
 INSERT INTO [Course](courseId, courseName, bccRelated) VALUES(1, 'Beginner', 1);
@@ -50,7 +100,6 @@ INSERT INTO [Course](courseId, courseName, bccRelated) VALUES(10, 'Presentation 
 SET IDENTITY_INSERT [Course] OFF;
 
 -- Employee table init --
-DELETE FROM [Employee];
 SET IDENTITY_INSERT[Employee] ON;
 
 INSERT INTO [EMPLOYEE](employeeId, accountName, password, fullName, gradeId, stream, email, active, locationId) VALUES (1, 'mitrais\admin', '$2a$10$fo87Tigk947EXQM9ya6BiO9HOk62x2ez8fpZ17jLqaOERveT0ivq2', 'Administrator', 5, '-', 'admin@example.com', 1, 1);
@@ -58,7 +107,6 @@ INSERT INTO [EMPLOYEE](employeeId, accountName, password, fullName, gradeId, str
 SET IDENTITY_INSERT [Employee] OFF;
 
 -- UserRole table init --
-DELETE FROM [UserRole];
 SET IDENTITY_INSERT[UserRole] ON;
 
 INSERT INTO [UserRole](userRoleId, employeeId, roleId) VALUES (1, 1, 1);
